7bc97f62bfbf215102cfc983c9c6dcb9e625ed76
tapo44ek <viktooooor2001@gmail.com>
2025-04-29 09:12:11 +0300

    only user_id required for endpoints now

--- backend/app/service/doccontrol.py
+++ backend/app/service/doccontrol.py
@@ -7,10 +7,11 @@ class DocService:
 
     async def get_docs_controls(self, params: dict) -> list[dict]:
         user_repository = UserRepository()
-        params['executor_name'] = await user_repository.get_user_fio_by_sedo_id(int(params['executor_sedo_id']))
-        params['boss1_name'] = await user_repository.get_user_fio_by_sedo_id(int(params['boss1_sedo_id']))
-        params['boss2_name'] = await user_repository.get_user_fio_by_sedo_id(int(params['boss2_sedo_id']))
-        params['boss3_name'] = await user_repository.get_user_fio_by_sedo_id(int(params['boss3_sedo_id']))
+        params = await user_repository.get_user_info_by_id(params['user_id'])
+        params['executor_name'] = await user_repository.get_user_fio_by_sedo_id(int(params['sedo_id']))
+        params['boss1_name'] = await user_repository.get_user_fio_by_sedo_id(int(params['boss1_sedo']) if params['boss1_sedo'] is not None else None)
+        params['boss2_name'] = await user_repository.get_user_fio_by_sedo_id(int(params['boss2_sedo']) if params['boss2_sedo'] is not None else None)
+        params['boss3_name'] = await user_repository.get_user_fio_by_sedo_id(int(params['boss3_sedo']) if params['boss3_sedo'] is not None else None)
 
 
 
