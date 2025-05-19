a6080e2f40d823920e9725bb04d807e796c47290
tapo44ek <viktooooor2001@gmail.com>
2025-05-19 16:38:59 +0300

    CORS edited 1

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,7 +17,9 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
                     "http://127.0.0.1:5173",
-                    "https://doccontrol.dsa.mlc.gov"
+                    "https://doccontrol.dsa.mlc.gov",
+                    "https://dsa.mlc.gov",
+                    "https://auth.dsa.mlc.gov"
                     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD", "TRACE", "CONNECT"],
